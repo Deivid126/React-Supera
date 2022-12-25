41983699f04466761c0df22f81187993bf54872e
Deivid Gonçalves <deividg2022@outlook.com>
2022-12-25 10:34:20 -0300

    initial config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import api from './Api';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+state={
+  transferencia:[]
+}
+
+async componentDidMount(){
+  const response = await api.get('');
+
+  console.log(response.data);
+
+  this.setState({transferencia: response.data})
+}
+  render(){
+    return(
+      <div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
