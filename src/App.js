3a97d3747c84d651cd4d2841d8bcdc0820ef8e04
Deivid Gonçalves <deividg2022@outlook.com>
2022-12-25 19:04:25 -0300

    alterações finais

--- src/App.js
+++ src/App.js
@@ -1,93 +1,102 @@
 import './App.css';
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import api from './Api';
+import { Table } from 'reactstrap';
 
 export default class App extends Component {
   state = {
-    transferencias:[]
+    transferencias: []
   };
 
-    
-
-      async componentDidMount(){
-
-        const form = document.querySelector('#formulario');
-        
-      
-        form.addEventListener('click',  async (event) => {
-
-         event.preventDefault();
-         
-         const DI = document.getElementById('DataStart');
-         const DE = document.getElementById('Dataend');
-         const NO = document.getElementById('nome');
-         
-         const DataInicio = DI.value;
-         const DataEnd = DE.value;
-         const Nome = NO.value;
-     
-     
-         let url = '';
-     
-         if(Nome || DataInicio){
-           if(DataInicio && Nome){
-             url = `/${DataInicio}/${DataEnd}/${Nome}`;
-           }
-           if(Nome){
-            url = `/${Nome}`;
-           }
-           if(!Nome) url = `/${DataInicio}/${DataEnd}`;
-         }
-         else url='';
 
 
-         const response =  await api.get(url).then(res => {
-          this.setState({transferencias:res.data})
-          console.log(res.data);
-         }).catch(console.error());
+  async componentDidMount() {
+
+    const form = document.querySelector('#formulario');
+
+
+    form.addEventListener('click', async (event) => {
+
+      event.preventDefault();
+
+      const DI = document.getElementById('DataStart');
+      const DE = document.getElementById('Dataend');
+      const NO = document.getElementById('nome');
+
+      const DataInicio = DI.value;
+      const DataEnd = DE.value;
+      const Nome = NO.value;
+
+
+      let url = '';
+
+      if (Nome || DataInicio) {
+        if (DataInicio && Nome) {
+          url = `/${DataInicio}/${DataEnd}/${Nome}`;
         }
-     )}
-    
-
-render(){
-     return (
-            <div>
-              <form id="formulario" action=''>
-                <div>
-                  <label for="DataStart">Data de Início:</label>
-                  <input type="text" id="DataStart" placeholder='2004-01-21' />
-                </div>
-                <div>
-                  <label for="Dataend">Data Final:</label>
-                  <input type="text" id="Dataend" placeholder='2004-01-30' />
-                </div>
-                <div>
-                  <label for="mnome">Nome do Operador da Transação:</label>
-                  <textarea type="text" id="nome"></textarea>
-                </div>
-                <div class="button">
-                  <button type="submit">Pesquisar</button>
-                </div>
-              </form>
-      
-              <div>
-                <li>
-                  {this.state.transferencias.map(transferencia => (
-                    <li>
-                      <b>Data:</b>{transferencia.data_transferencia}<br/>
-                      <b>Valor:</b>{transferencia.valor}<br/>
-                      <b>Tipo:</b>{transferencia.tipo}<br/>
-                      <b>Nome do Operador:</b>{transferencia.nome_operador_transacao}<br/>
-                    </li>
-                  ))}
-                </li>
-              </div>
-            </div>
-          );
-
-                  }
-
-
-
+        if (Nome) {
+          url = `/${Nome}`;
+        }
+        if (!Nome) url = `/${DataInicio}/${DataEnd}`;
       }
+      else url = '';
+
+
+      const response = await api.get(url).then(res => {
+        this.setState({ transferencias: res.data })
+        console.log(res.data);
+      }).catch(console.error());
+    }
+    )
+  }
+
+
+  render() {
+    return (
+      <div id='body'>
+        <form id="formulario" action=''>
+          <div>
+            <label for="DataStart">Data de Início:</label>
+            <input type="text" id="DataStart" placeholder='2004-01-21' />
+          </div>
+          <div>
+            <label for="Dataend">Data Final:</label>
+            <input type="text" id="Dataend" placeholder='2004-01-30' />
+          </div>
+          <div>
+            <label for="mnome">Nome do Operador da Transação:</label>
+            <input type="text" id="nome"></input>
+          </div>
+          <div class="button">
+            <button type="submit">Pesquisar</button>
+          </div>
+        </form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Valor</th>
+              <th>Tipo</th>
+              <th>Nome</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.transferencias.map(transferencia => (
+        <tr>
+          <td>{transferencia.data_transferencia}</td>
+          <td>{transferencia.valor}</td>
+          <td>{transferencia.tipo}</td>
+          <td>{transferencia.nome_operador_transacao || transferencia.id_conta.nome_responsavel}</td>
+        </tr>
+          ))}
+        </tbody>
+        </Table>
+      </div>
+    );
+
+  }
+
+
+
+}
 
